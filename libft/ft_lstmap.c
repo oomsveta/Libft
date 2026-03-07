bcfbefcbeb551dc48e194587647e7a116e5181ea
Louis Wicket <louis.wicket@protonmail.com>
2026-03-08 00:24:23 +0100

    properly free node content in case of node allocation failure

--- libft/ft_lstmap.c
+++ libft/ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: lwicket <lwicket@student.42belgium.be>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/08 00:02:05 by lwicket           #+#    #+#             */
-/*   Updated: 2026/03/08 00:14:34 by lwicket          ###   ########.fr       */
+/*   Updated: 2026/03/08 00:22:46 by lwicket          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,17 @@ t_list *ft_lstmap(t_list *lst, void *(*fn)(void *), void (*delete_fn)(void *))
 	t_list	*mapped;
 	t_list	*new_node;
 	t_list	**tail;
+	void	*new_content;
 
 	mapped = NULL;
 	tail = &mapped;
 	while (lst)
 	{
-		new_node = ft_lstnew(fn(lst->content));
+		new_content = fn(lst->content);
+		new_node = ft_lstnew(new_content);
 		if (!new_node)
 		{
+			delete_fn(new_content);
 			ft_lstclear(&mapped, delete_fn);
 			return (NULL);
 		}
