0cdf3358ae535d9e6837d4348f5a088d9e51b577
Louis Wicket <louis.wicket@protonmail.com>
2026-03-08 19:48:30 +0100

    fix: don't use memcpy in case of overlapping buffers

--- libft/ft_memmove.c
+++ libft/ft_memmove.c
@@ -6,11 +6,26 @@
 /*   By: lwicket <lwicket@student.42belgium.be>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/02 09:22:10 by lwicket           #+#    #+#             */
-/*   Updated: 2026/03/08 17:40:24 by lwicket          ###   ########.fr       */
+/*   Updated: 2026/03/08 19:47:30 by lwicket          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"	// provides ft_memcpy, size_t
+
+static inline void	*copy_ascending(
+	unsigned char *dest, const unsigned char *src, size_t n
+)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < n)
+	{
+		dest[i] = src[i];
+		i += 1;
+	}
+	return (dest);
+}
 
 static inline void	*copy_descending(
 	unsigned char *dest, const unsigned char *src, size_t n
@@ -30,9 +45,13 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 	{
 		return (dest);
 	}
-	if ((uintptr_t)dest - (uintptr_t)src < n)
+	if ((uintptr_t)src - (uintptr_t) dest - n <= -2 * n)
+	{
+		return (ft_memcpy(dest, src, n));
+	}
+	if ((uintptr_t)dest > (uintptr_t)src)
 	{
 		return (copy_descending(dest, src, n));
 	}
-	return (ft_memcpy(dest, src, n));
+	return (copy_ascending(dest, src, n));
 }
