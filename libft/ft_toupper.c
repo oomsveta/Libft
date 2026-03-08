82957faa85bc23ee8ff1e2e8832008bccec40f43
Louis Wicket <louis.wicket@protonmail.com>
2026-03-08 18:53:57 +0100

    fix: use xor instead of or

--- libft/ft_toupper.c
+++ libft/ft_toupper.c
@@ -6,7 +6,7 @@
 /*   By: lwicket <lwicket@student.42belgium.be>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/01 18:54:04 by lwicket           #+#    #+#             */
-/*   Updated: 2026/03/01 18:59:03 by lwicket          ###   ########.fr       */
+/*   Updated: 2026/03/08 18:53:46 by lwicket          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int	ft_toupper(int c)
 {
 	if (ft_islower(c))
 	{
-		return (c | 0x20);
+		return (c ^ 0x20);
 	}
 	return (c);
 }
