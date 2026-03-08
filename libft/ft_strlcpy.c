59f58d855b1ddfc5cb585544ef05bca061b9c98f
Louis Wicket <louis.wicket@protonmail.com>
2026-03-08 17:44:55 +0100

    remove useless stpcpy

--- libft/ft_strlcpy.c
+++ libft/ft_strlcpy.c
@@ -6,21 +6,11 @@
 /*   By: lwicket <lwicket@student.42belgium.be>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/01 19:24:04 by lwicket           #+#    #+#             */
-/*   Updated: 2026/03/01 19:38:26 by lwicket          ###   ########.fr       */
+/*   Updated: 2026/03/08 17:44:48 by lwicket          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-char	*ft_stpcpy(char *dest, const char *src)
-{
-	while (*src != '\0')
-	{
-		*dest++ = *src++;
-	}
-	*dest = '\0';
-	return (dest);
-}
 
 size_t	ft_strlcpy(char *dest, const char *src, size_t size)
 {
