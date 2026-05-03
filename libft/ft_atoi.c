5c907b29e24e5c99cf558a2d7e5e031e8862bb9b
Louis <louis.wicket@protonmail.com>
2026-05-03 20:07:19 +0200

    use XOR to compute to compute the digit

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: lwicket <lwicket@student.42belgium.be>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/08 16:40:31 by lwicket           #+#    #+#             */
-/*   Updated: 2026/03/08 16:45:37 by lwicket          ###   ########.fr       */
+/*   Updated: 2026/05/03 20:06:46 by lwicket          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,17 +17,17 @@ int	ft_atoi(const char *nptr)
 {
 	bool			is_negative;
 	unsigned int	acc;
-	unsigned char	digit;
+	unsigned int	digit;
 
 	nptr += ft_strspn(nptr, " \n\t\r\v\f");
 	is_negative = *nptr == '-';
 	nptr += is_negative || *nptr == '+';
 	acc = 0;
-	digit = (unsigned char)*nptr++ - '0';
-	while (digit < 10u)
+	digit = *nptr++ ^ '0';
+	while (digit < 10)
 	{
 		acc = acc * 10 + digit;
-		digit = (unsigned char)*nptr++ - '0';
+		digit = *nptr++ ^ '0';
 	}
 	if (is_negative)
 	{
